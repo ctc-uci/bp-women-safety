e20e36215b1b6eb552b48924269b436f4d60c030
Rayan Tighiouart <t.alex.2002@gmail.com>
2022-05-15 14:25:36 -0700

    pull requesting for access to new card component

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import BrandName from '../components/BrandName';
 import Footer from '../components/Footer';
-/*add link later*/
+import { Link } from 'react-router-dom';
+import './Layout.css';
 
 const Layout = () => {
   return (
   <>
     <nav>
       <p>Navbar</p>
+      <Link to="" className="link">Home Page  </Link>
+      <Link to="/Resources" className="link">All Resources Page  </Link>
     </nav>
     <Outlet />
     <div className="Footer">
