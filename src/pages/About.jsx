529b088793dddd60c50b8973df24973fb4b24bf8
Rayan Tighiouart <t.alex.2002@gmail.com>
2022-05-15 16:18:34 -0700

    About page started

---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './About.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const About = () => {
+return (
+  <>
+    <div className="top">
+      <h3 div='font2'>ABOUT US</h3>
+      <p>Making campus resoruces more accessible and easily digestible</p>
+    </div>
+  </>
+);
+}
+
+
+export default About;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import BrandName from '../components/BrandName';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+import About from "./About.jsx";
+import './Layout.css';
 /*add link later*/
 
 const Layout = () => {
@@ -8,6 +11,8 @@ const Layout = () => {
   <>
     <nav>
       <p>Navbar</p>
+      <Link to="" className="link">Home Page</Link>
+      <Link to="/About" element={<About/>} className="link">About Page</Link>
     </nav>
     <Outlet />
     <div className="Footer">
