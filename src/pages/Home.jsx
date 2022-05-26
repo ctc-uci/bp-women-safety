93b803acd76ef8c4639f7a5fbd3f23606c43511b
Rayan Tighiouart <t.alex.2002@gmail.com>
2022-05-26 13:35:10 -0700

    merge!!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import Map from '../components/Map';
-import Feature from '../components/Feature';
+import ResourceCard from '../components/ResourceCard.jsx';
 import './Home.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+import cardimg1 from '../images/WomxnCenterPic.jpg'
+import cardimg2 from '../images/FreshHubPic.jpg'
+import cardimg3 from '../images/BlueLight.jpg'
+
+const cardlink1 = "https://womxnscenter.uci.edu/"
+const cardlink2 = "https://basicneeds.uci.edu/"
+const cardlink3 = "https://www.police.uci.edu/how-do-i/_brochures/bluelight_phones.pdf"
+
+const cardtxt1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur enim fringilla volutpat in. Nam volutpat leo rhoncus facilisi amet ante. Nibh elit massa pellentesque."
+const cardtxt2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur enim fringilla volutpat in. Nam volutpat leo rhoncus facilisi amet ante. Nibh elit massa pellentesque."
+const cardtxt3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur enim fringilla volutpat in. Nam volutpat leo rhoncus facilisi amet ante. Nibh elit massa pellentesque."
+
 
 const Home = () => {
   return (
@@ -9,7 +25,25 @@ const Home = () => {
       <div className="MapBody">
         <Map />
       </div>
-      <Feature />
+      <div className="cardBody">
+        <h1>Featured Resources</h1>
+        <Container>
+        <Row>
+          <div className="row">
+            <div className="col1">
+              <ResourceCard link={cardlink1} img={cardimg1} title="Womxn&apos;s Center for Success" txt={cardtxt1}/>
+            </div>
+            <div className="col2">
+              <ResourceCard link={cardlink2} img={cardimg2} title="Fresh Hub" txt={cardtxt2}/>
+            </div>
+            <div className="col3">
+              <ResourceCard link={cardlink3} img={cardimg3} title="Emergency Poles" txt={cardtxt3}/>
+            </div>
+          </div>
+        </Row>
+        </Container>
+      </div>
+
     </div>
   );
 };
