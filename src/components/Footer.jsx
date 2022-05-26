df8b0f447019cafdb63a8483147d68d7092e94fa
Rayan Tighiouart <t.alex.2002@gmail.com>
2022-05-26 13:06:17 -0700

    navbar added, minor fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossOrigin="anonymous"
       />
-
+      <div className="footer">
       <Container>
         <Row>
           <Col>
@@ -71,6 +71,7 @@ function Footer() {
           </Col>
         </Row>
       </Container>
+      </div>
     </>
   );
 }
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,18 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import BrandName from '../components/BrandName';
 import Footer from '../components/Footer';
-import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 import './Layout.css';
 
 const Layout = () => {
   return (
   <>
-    <nav>
-      <p>Navbar</p>
-      <Link to="" className="link">Home Page</Link>
-      <Link to="/Resources" className="link">All Resources Page</Link>
-      <Link to="/About" className="link">About Page</Link>
-    </nav>
+    <Navbar />
     <Outlet />
     <div className="Footer">
       <Footer />
